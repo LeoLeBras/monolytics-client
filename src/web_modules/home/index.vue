e327d34624dbb848fe6c9c7bd709b556291f0a81
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-04-14 23:54:38 +0200

    Start cleaning home

--- src/web_modules/home/index.vue
+++ src/web_modules/home/index.vue
@@ -1,55 +1,48 @@
 <template>
   <app-header></app-header>
-    <div class="home_part">
-      <div class="video_container"> 
-      <div class="video_mask">
-        <div id="video_effect"></div>
-          <div id="video_trailers">
-            <video class="video" autoplay loop>
-                <source src="../src/assets/medias/trailers/movies.webm" type="video/webm">
-              Your browser does not support HTML5 video.  
-            </video>
-          </div>
-      </div>
+  <div class="home_part">
+    <div class="video_container">
+    <div class="video_mask">
+      <div id="video_effect"></div>
+        <div id="video_trailers">
+          <video class="video" autoplay loop>
+              <source src="../src/assets/medias/trailers/movies.webm" type="video/webm">
+            Your browser does not support HTML5 video.
+          </video>
+        </div>
     </div>
-    <div class="col-md-12 text_home">
-       <div class="catch_text">
-         We rank up the latest popular movies
+  </div>
+  <div class="col-md-12 text_home">
+     <div class="catch_text">
+       We rank up the latest popular movies
+       <br />
+       with our <span class="brand_rank">trurank</span> notation system
+     </div>
+     <div class="scroll_down">
+       <div class="text_s_d">
+         scroll down
          <br />
-         with our <span class="brand_rank">trurank</span> notation system
+         to discover the tops
        </div>
-     
-       <div class="scroll_down">
-         <div class="text_s_d">
-           scroll down
-           <br />
-           to discover the tops
-         </div>
-     
-         <div class="icone_s_d">
-          <div class="mouse"></div>
-          <div class="mouse_button"></div>
-          <div class="mouse_roll"></div>
-         </div>
+       <div class="icone_s_d">
+        <div class="mouse"></div>
+        <div class="mouse_button"></div>
+        <div class="mouse_roll"></div>
        </div>
      </div>
-     
-     
-       <div class="col-md-12 twitter_slide">
-         <div class="timeline_twitter">
-            <ul class="text-slider">
-             <li class="text-slide"><a href="">#NW deadpool, see what all that fuzz is about...</a></li>
-             <li class="text-slide"><a href="">#NW Zootopia with the kids !</a></li>
-             <li class="text-slide"><a href="">#NW BatmanvsSuperman, what a load of crap.</a></li>
-             <li class="text-slide"><a href="">#NW Thebookofthejungle, all my childhood :'-)</a></li>
-             <li class="text-slide"><a href="">#NW Rio with GF</a></li>
-          </ul>
-         </div>
-       </div>
-
-       <nav>
-    </div>
- 
+   </div>
+   <!-- <div class="col-md-12 twitter_slide">
+     <div class="timeline_twitter">
+        <ul class="text-slider">
+         <li class="text-slide"><a href="">#NW deadpool, see what all that fuzz is about...</a></li>
+         <li class="text-slide"><a href="">#NW Zootopia with the kids !</a></li>
+         <li class="text-slide"><a href="">#NW BatmanvsSuperman, what a load of crap.</a></li>
+         <li class="text-slide"><a href="">#NW Thebookofthejungle, all my childhood :'-)</a></li>
+         <li class="text-slide"><a href="">#NW Rio with GF</a></li>
+      </ul>
+     </div>
+   </div> -->
+  </div>
 </template>
 
 <script>
@@ -90,7 +83,7 @@
 
   @import "../theme";
   @import "./style";
-  
+
 
 
 </style>
